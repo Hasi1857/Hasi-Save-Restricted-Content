5c8a616519f5e8a9da897f10abe6083c763f1be1
Hasi1857 <hasi185778@gmail.com>
2025-12-04 18:54:48 +0500

    Update config.py

--- config.py
+++ config.py
@@ -23,12 +23,12 @@ API_ID = int(os.environ.get("API_ID", "37785994"))
 API_HASH = os.environ.get("API_HASH", "0ea27c6c789f105e0a1c53f6b938a18d")
 
 # Your Owner / Admin Id For Broadcast 
-ADMINS = int(os.environ.get("ADMINS", "7458547996"))
+ADMINS = int(os.environ.get("ADMINS", "8588075938"))
 
 # Your Channel Id In Which Bot Upload Downloaded Video/File/Message etc.
 # And Make Your Bot Admin In this channel with full rights.
 # if you don't want to upload in channel then leave it blank don't fill anything.
-CHANNEL_ID = os.environ.get("CHANNEL_ID", "-1003430062201")
+CHANNEL_ID = os.environ.get("CHANNEL_ID", "-1003495619252")
 
 # Your Mongodb Database Url
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
